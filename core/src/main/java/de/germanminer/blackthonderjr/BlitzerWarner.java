5c8a220e27acd7d8b84a1c44b78980837f3d0117
BlackThonder_Jr <blackthonder.jr@outlook.de>
2024-05-17 18:49:09 +0200

    Web Blitzer Datei Als Auslese Quelle

--- core/src/main/java/de/germanminer/blackthonderjr/BlitzerWarner.java
+++ core/src/main/java/de/germanminer/blackthonderjr/BlitzerWarner.java
@@ -4,6 +4,10 @@ import de.germanminer.blackthonderjr.listener.BlitzerKeys;
 import net.labymod.api.addon.LabyAddon;
 import net.labymod.api.models.addon.annotation.AddonMain;
 import de.germanminer.blackthonderjr.listener.BlitzerListener;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -17,35 +21,34 @@ public class BlitzerWarner extends LabyAddon<BlitzerConfiguration> {
   @Override
   protected void enable() {
     prefix = configuration().prefix().get().toString().replace("&", "§") + " ";
+    loadBlitzer();
     this.registerSettingCategory();
 
     this.registerListener(new BlitzerListener(this));
     this.registerListener(new BlitzerKeys(this));
     BlitzerListener.isInRange = false;
-
-    Koords.add("-1927 73 -1383 Hensburg 50");
-    Koords.add("-2137 65 -1437 Hensburg-Westend(Tunnel) 50");
-    Koords.add("-607 67 -2268 Versicherungsviertel 70");
-    Koords.add("-2437 66 -2197 Westend(Flughafen) 30");
-    Koords.add("-1597 67 -1670 JVA_Spegelsbach(Autovermietung) 50");
-    Koords.add("-788 67 -2209 Offenbach_Ost(Richtung_Hbf) 50");
-    Koords.add("-2026 71 -831 Hohenbrueck 50");
-    Koords.add("-1866 73 -1039 Niebrück 80");
-    Koords.add("-2917 59 -1017 Westend-Niebrück(im_Tunnel) 100");
-    Koords.add("-2917 64 -717 Niebrück(Tunnel_Ausfahrt) 50");
-    Koords.add("-2421 69 1037 Niebrück(Haudi_Autohaus) 70");
-    Koords.add("-2505 70 258 Goldschweig(Einkaufsmeile) 50");
-    Koords.add("-1235 70 -889 Harrenheim-GwG-Ost(Bahnübergang) 50");
-    Koords.add("-1533 80 -82 Kraftwerk(Richtung_GwG_Ost) 100");
-    Koords.add("152 66 -2121 Universität 30");
-    Koords.add("-273 67 -1389 Zentrum(Passage_Hochhaus) 50");
-    Koords.add("-1476 67 -1992 Offenbach_Süd 50");
-    Koords.add("-2723 70 364 Verwahrstelle 50");
-
   }
 
   @Override
   protected Class<BlitzerConfiguration> configurationClass() {
     return BlitzerConfiguration.class;
+  }
+
+  public void loadBlitzer(){
+    try{
+      URL blitzer = new URL("http://awantalife.de/Blitzer.txt");
+
+      URLConnection conBlitzer = blitzer.openConnection();
+      BufferedReader br = new BufferedReader(new InputStreamReader( conBlitzer.getInputStream()));
+      String strLine = "";
+      while ( ( strLine = br.readLine() ) != null)
+        Koords.add(strLine);
+      br.close();
+
+    }
+    catch(Exception ex)
+    {
+      ex.printStackTrace();
+    }
   }
 }
