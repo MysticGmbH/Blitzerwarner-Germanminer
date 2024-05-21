a5cc4d188b23a280772aa357f1667587237cb29e
BlackThonder_Jr <blackthonder.jr@outlook.de>
2024-05-21 21:46:45 +0200

    Fix2

--- core/src/main/java/de/germanminer/blackthonderjr/listener/BlitzerKeys.java
+++ core/src/main/java/de/germanminer/blackthonderjr/listener/BlitzerKeys.java
@@ -4,6 +4,8 @@ import de.germanminer.blackthonderjr.BlitzerWarner;
 import net.labymod.api.Laby;
 import net.labymod.api.client.component.Component;
 import net.labymod.api.client.component.format.NamedTextColor;
+import net.labymod.api.client.component.format.TextColor;
+import net.labymod.api.client.component.format.TextDecoration;
 import net.labymod.api.event.Subscribe;
 import net.labymod.api.event.client.input.KeyEvent;
 
@@ -19,7 +21,8 @@ public class BlitzerKeys {
       if(e.state().name().equalsIgnoreCase("PRESS")){
         if(e.key().getTranslationKey().equalsIgnoreCase(this.addon.configuration().toggleBind().get().getTranslationKey())){
           String soundStatus = this.addon.configuration().all().get().toString();
-          this.addon.displayMessage(BlitzerWarner.prefix.append(Component.text("Die Benachrichtigungen wurden ", NamedTextColor.GRAY)).append(soundStatus.equals("true") ? Component.text("Deaktiviert!",
+          this.addon.displayMessage(Component.text(addon.configuration().prefix().get().toString(), TextColor.color(addon.configuration().prefixColor().get())).decorate(
+              TextDecoration.BOLD).append(Component.text(" Die Benachrichtigungen wurden ", NamedTextColor.GRAY)).append(soundStatus.equals("true") ? Component.text("Deaktiviert!",
               NamedTextColor.RED) : Component.text("Aktiviert!", NamedTextColor.GREEN)));
           this.addon.configuration().all().set(!this.addon.configuration().all().get());
         }
